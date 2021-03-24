0c95232776728c2a950bb26176f23ac6a2c94016
Ashraf <Ashraf.mail92@gmail.com>
2021-03-24 15:27:31 +0100

    made the app responsive to the screen size

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { relative } from 'node:path';
 import Box from './components/Box/Box'
 import Grid from './components/Grid/Grid';
 import GridRuler from './components/GridRuler/GridRuler';
@@ -7,26 +6,57 @@ function App() {
   return (
     <div className="App">
 
-      <GridRuler spacing="md"></GridRuler>
-      
-      <Grid container spacing="md">
-
-        <Grid item cols={6}>
-          <Box>Box 1</Box>
+      <Grid container spacing="lg">
+        <GridRuler spacing="lg"></GridRuler>
+        
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 1</Box>
         </Grid>
         
-        <Grid item cols={6}>
-          <Box>Box 2</Box>
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 2</Box>
         </Grid>
 
-        <Grid item cols={6}>
-          <Box>Box 3</Box>
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 3</Box>
         </Grid>
 
-
-        <Grid item cols={6}>
-          <Box>Box 4</Box>
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 4</Box>
         </Grid>
+
+<Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 5</Box>
+        </Grid>
+        
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box>6</Box>
+        </Grid>
+        
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 7</Box>
+        </Grid>
+        
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 8</Box>
+        </Grid>
+        
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 9</Box>
+        </Grid>
+        
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 10</Box>
+        </Grid>
+        
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 11</Box>
+        </Grid>
+        
+        <Grid item xs={12} sm={6} md={3} lg={1}>
+          <Box> 12</Box>
+        </Grid>
+
       </Grid>
     </div>
   );
